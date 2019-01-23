9c13c9e337ec229db459f86011f1f9b032c1c5ee
Suresh <soori_rozee@yahoo.com>
2019-01-23 15:30:04 +0530

    Initial commit

---
+++ application/views/flat/1-Home/index.php
@@ -0,0 +1,44 @@
+<div class="container-fluid flat-page firstDiv">
+	<div class="row justify-content-center overlay">
+		<div class="col-md-12 align-self-center">
+			<div class="container">
+				<h2>ಕೃತಿಗಳು</h2><br />
+				<div class="row">
+					<div class="card-columns">
+						<div class="card bg-light">
+							<div class="card-body text-center">
+								<p class="card-text"><a href="<?=BASE_URL?>krutigalu/001">ಸ್ವತಂತ್ರ ಗ್ರಂಥಗಳು</a></p>
+							</div>
+						</div>
+						<div class="card bg-light">
+							<div class="card-body text-center">
+								<p class="card-text"><a href="<?=BASE_URL?>krutigalu/002">ಅನುವಾದ ಗ್ರಂಥಗಳು</a></p>
+							</div>
+						</div>
+						<div class="card bg-light">
+							<div class="card-body text-center">
+								<p class="card-text"><a href="<?=BASE_URL?>krutigalu/003">ವ್ಯಾಖ್ಯಾನ ಗ್ರಂಥಗಳು</a></p>
+							</div>
+						</div>
+						<div class="card bg-light">
+							<div class="card-body text-center">
+								<p class="card-text"><a href="<?=BASE_URL?>krutigalu/004">ಸಂಪಾದಿತ ಗ್ರಂಥಗಳು</a></p>
+							</div>
+						</div>  
+						<div class="card bg-light">
+							<div class="card-body text-center">
+								<p class="card-text"><a href="<?=BASE_URL?>krutigalu/005">ಸಹಕರ್ತೃ ಗ್ರಂಥಗಳು</a></p>
+							</div>
+						</div>
+						<div class="card bg-light">
+							<div class="card-body text-center">
+								<p class="card-text"><a href="<?=BASE_URL?>krutigalu/006">ಲೇಖನಗಳು</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<a class="homepage scrollMore goTo" data-destination="#wordOfTheDay"><span></span>&nbsp;</a>
+	</div>
+</div>
